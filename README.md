95797bcc193fb0db7632a4732f4059bdf2702406
eimearturner <eimeardt@hotmail.com>
2017-12-03 22:02:39 +0000

    fixed read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Cloud2-
+GETS:
+go to the myapp folder
+run python3 app.py
+go to compute engine and click managers ip
+tack port 5000 to the url to see the commands that can be run
+add appropriate method names to run them
+POST/PATCH/DEL:
+run the python3 app.py as before in one SSH window
+open another and run the curl commands individually as specified in the commands.sh bash file
+
--- commands.sh
+++ commands.sh
@@ -1,30 +1,30 @@
-//creating a docker image
+#creating a docker image
 curl -H 'Accept: application/json' -F "file=@dockerfiles/whale-say.Dockerfile" http://35.195.27.0:5000/images
 
-//creating a docker container
+#creating a docker container
 curl -X POST -H "Content-Type: application/json" http://35.195.27.0:5000/containers -d '{"image": "dockersamples/visualizer:latest"}'
 
-//get all containers
+#get all containers
 curl -s -X GET -H 'Accept: application/json' http://http://35.195.27.0:5000/containers
 
-//get all containers that are running
+#get all containers that are running
 curl -s -X GET -H 'Accept: application/json' http://35.195.27.0:5000/containers?state=running
 
-//change container state
+#change container state
 curl -X PATCH -H "Content-Type: application/json" http://35.195.27.0:5000/containers/69a8dd28c359 -d '{"state": "running"}'
 curl -X PATCH -H "Content-Type: application/json" http://35.195.27.0:5000/containers/69a8dd28c359 -d '{"state": "stopped"}'
 
-//change image tag
+#change image tag
 curl -s -X PATCH -H curl -X PATCH -H "Content-Type: application/json" http://35.195.27.0:5000/images/656c72e5b88f -d '{"tag": "test:1.1"}'
 
-//del container by its id
+#del container by its id
 curl -s -X DELETE -H "Content-Type: application/json" http://35.195.27.0:5000/containers/e5b8193b0834
 
-//del image by its id
+#del image by its id
 curl -s -X DELETE -H "Content-Type: application/json" http://35.195.27.0:5000/images/8e603db2581b
 
-//del all containers
+#del all containers
 curl -s -X DELETE -H 'Accept: application/json' http://35.195.27.0:5000/containers
 
-//del all images 
+#del all images 
 curl -s -X DELETE -H 'Accept: application/json' http://35.195.27.0:5000/images
